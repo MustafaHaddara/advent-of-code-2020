b1d309412d173589c0387b04fb0a3f5f8d00ed40
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2020-12-01 19:09:04 -0500

    day 1-2: complicated accounting

---
+++ ts/day1-2.ts
@@ -0,0 +1,20 @@
+import { readInputFileLines } from './base';
+
+const main = () => {
+    const lines = readInputFileLines();
+
+    const nums: number[] = lines
+        .map(line => parseInt(line));
+
+    for (let i=0; i<nums.length; i++) {
+        for (let j=i+1; j<nums.length; j++) {
+            for (let k=j+1; k<nums.length; k++) {
+                if (nums[i] + nums[j] + nums[k] === 2020) {
+                    return nums[i]*nums[j]*nums[k];
+                }
+            }
+        }
+    }
+}
+
+console.log(main());
