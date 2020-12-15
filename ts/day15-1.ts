a283e2a3f1e0cd836db6ac02e0137f64881c9da4
Mustafa Haddara <mustafaayhaddara@yahoo.com>
2020-12-15 00:35:14 -0500

    15-1: memory game

---
+++ ts/day15-1.ts
@@ -0,0 +1,28 @@
+import { SolveFunc } from './types';
+
+export const solve: SolveFunc = (lines) => {
+  const nums = lines[0].split(',').map((l) => parseInt(l));
+
+  const spoken = {};
+  const second = {};
+  const end = 2020;
+  let i = 0;
+  let last = -1;
+  while (i < end) {
+    if (i < nums.length) {
+      spoken[nums[i]] = i;
+      last = i;
+    } else {
+      const diff = second[last] === undefined ? 0 : spoken[last] - second[last];
+      if (spoken[diff] !== undefined) {
+        second[diff] = spoken[diff];
+      }
+      spoken[diff] = i;
+      last = diff;
+    }
+    i++;
+  }
+  return last;
+};
+
+export const testInput = ['0,3,6'];
